1e56e743d4f95c394e52517e4290086b7d48cd57
Robert Holland <robertholland@Roberts-MBP-2.attlocal.net>
2026-02-25 08:35:03 -0600

    Add mobile concept

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import MobileConcept from './MobileConcept'
+
+const isMobile = window.location.hash === '#mobile'
+
 ReactDOM.createRoot(
- document.getElementById('root')
+  document.getElementById('root')
 ).render(
- <React.StrictMode>
- <App />
- </React.StrictMode>
+  <React.StrictMode>
+    {isMobile ? <MobileConcept /> : <App />}
+  </React.StrictMode>
 )
+
+window.addEventListener('hashchange', () => location.reload())
